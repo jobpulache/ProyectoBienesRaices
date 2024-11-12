40d62a7b9a777eb1d0024b244ddcb31546bfe229
Job <pulachecarrenojob837@gmail.com>
2024-11-11 23:49:51 -0500

    RamdomCommit1

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,26 @@ document.addEventListener('DOMContentLoaded', function(){
     darkMode();
 });
 function darkMode(){
+    const prefiereDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+   
+
+     if(!prefiereDarkMode.matches){
+         document.body.classList.add('dark-mode')//Af
+         console.log('Prefiere dark mode matches');
+     }else{
+         document.body.classList.remove('dark-mode')
+     }
+
+     //Cmbue en automatico, dependiendo de las preferencias o configuraciones que tenga el usuario en su navegador.
+    prefiereDarkMode.addEventListener('change',function(){
+        if(prefiereDarkMode.matches){
+            document.body.classList.add('dark-mode')//Af
+            console.log('Prefiere dark mode matches');
+        }else{
+            document.body.classList.remove('dark-mode')
+        }
+    })
+
     const botonDarkMode= document.querySelector('.dark-mode-boton');
     botonDarkMode.addEventListener('click', function(){
         document.body.classList.toggle('dark-mode')
