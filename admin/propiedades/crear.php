cd7df2f4066b202f9d864385528cc3f4a37ab61d
Job <pulachecarrenojob837@gmail.com>
2024-12-01 15:02:04 -0500

    creating carp imagenes and upload imagenes

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -24,19 +24,43 @@ $vendedorId = '';
 
 //Ejecutar el código después de que el usuario envía el form.
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    echo '<pre>';
-    var_dump($_POST);
-    echo '</pre>';
-    //Pasamos todos los campos al metodo post
-    //Para que nos muestre los datos 
-    $titulo = $_POST['titulo']; //Les asignamos el contenido
-    $precio = $_POST['precio'];
-    $descripcion = $_POST['descripcion']; //A estas variables les asignamos el contenido del post
-    $habitaciones = $_POST['habitaciones'];
-    $wc = $_POST['wc'];
-    $estacionamiento = $_POST['estacionamiento'];
-    $vendedorId = $_POST['vendedor'];
-    $creado = date('Y/m/d');
+
+
+    //Ejemplo de sanitización
+    // $numero = "1HOLA";
+    // $numero2 = 1;
+    // $correo = "hola@gmail.com";
+    // //sanitizar
+    // $resultado = filter_var($numero, FILTER_SANITIZE_NUMBER_INT); //Sanitizando el número uno - Mantiene solo los números enteros 
+    // //Usando estos filtros podemos sanitizar o eliminar lo que no requerimos
+    // //Validar que sea un entero
+    // $resultado = filter_var($numero2, FILTER_VALIDATE_INT); //Valida que sea un entero 
+    // $resultado = filter_var($numero, FILTER_VALIDATE_EMAIL);
+
+    // echo '<pre>';
+    // var_dump($_POST);
+    // echo '</pre>';
+
+    // echo '<pre>';
+    // var_dump($_FILES); //Es la super global de archivos - lee archivos(puede ser img)
+    // echo '</pre>';
+
+    //Pasamos todos los campos al metodo post Para que nos muestre los datos 
+    $titulo = mysqli_real_escape_string($db,  $_POST['titulo']); //Les asignamos el contenido
+    $precio = mysqli_real_escape_string($db,  $_POST['precio']);
+    $descripcion = mysqli_real_escape_string($db,  $_POST['descripcion']); //A estas variables les asignamos el contenido del post
+    $habitaciones = mysqli_real_escape_string($db,  $_POST['habitaciones']);
+    $wc = mysqli_real_escape_string($db,  $_POST['wc']);
+    $estacionamiento = mysqli_real_escape_string($db,  $_POST['estacionamiento']); //Variable- function-bd-method=post-campoBD
+    $vendedorId = mysqli_real_escape_string($db,  $_POST['vendedor']);
+    $creado = date('Y/m/d'); //Año, mes y día
+
+    //Asignar files hacia una variable
+    $imagen = $_FILES['imagen']; //Accedemos con el name, con el name es como obtenemos los datos ya en el servidor
+
+
+    // var_dump($imagen['name']);//En caso que el valor exista, significa que el usuario si subio la imagen
+    // exit;
 
     //Si el titulo está vacio. Lo vamos agregando al arrelglo de errores
     //[CORCHETES]
@@ -62,6 +86,17 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (!$vendedorId) {
         $errores[] = 'Elige un vendedor';
     }
+    if (!$imagen['name'] || $imagen['error']) {
+        $errores[] = "La imagen es obligatorio"; //La agregamos a errores eb caso no haya subido la imagen
+    }
+
+    //Validamos el tamaño(1mb max.)
+    $medida = 1000 * 1000; //Formula de medida, convertimos de bits a kbits
+
+    if ($imagen['size'] > $medida) {
+        $errores[] = 'La imagen es muy pesada'; //La add at errores in caso se exceda - validamos poe tamaño
+    }
+
     // echo '<pre>';
     // var_dump($errores);//Podemos visualuzar los errores
     // echo '</pre>';
@@ -74,13 +109,33 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     //empty revisa que un array este vacio
     if (empty($errores)) {
 
+        /*Upload de archivos*/
+
+
+        //Creating carpt
+        $carpetaImagenes = '../../imagenes'; //Cuando no habea errores, va a crear esa carpeta.
+
+
+        if (!is_dir($carpetaImagenes)) { //This function return if carpt exists or not(is_dir)
+            mkdir($carpetaImagenes); //Esta función it's for creating directory
+        };
+
+
+        //Subir imagen - exits one function7
+        move_uploaded_file($imagen['tmp_name'], $carpetaImagenes."/archivo.jpg");
+
+        exit; //Para   que no insert in BD
+
+
         $query = "INSERT INTO propiedades(titulo, precio, descripcion, habitaciones, wc, estacionamiento,creado, Vendedores_id)
         VALUES ('$titulo',
-        '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamiento','$creado', '$vendedorId')";
+        '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamiento','$creado', '$vendedorId')"; //Add creado
 
         $resultado = mysqli_query($db, $query);
         if ($resultado) {
-            echo 'Insertado correctamente';
+            // echo 'Insertado correctamente';
+            //Mejor hacemos que nos redireccione, así evitamos que los usuarios piensen que no se registraron los datos
+            header('Location: /admin'); //Header es una función para redireccionar a un usuario
         } else {
             echo 'No se logro';
         }
@@ -103,7 +158,8 @@ incluirTemplate('header');
             <?php echo $error ?>
         </div>
     <?php endforeach; ?>
-    <form class="formulario-contacto" method="POST" action="/admin/propiedades/crear.php">
+    <!--Cuando un form quiee subir archivos se debe agregar: enctype="multipart/form-data". Para que lea archivos-->
+    <form class="formulario-contacto" method="POST" action="/admin/propiedades/crear.php" enctype="multipart/form-data">
         <fieldset>
             <legend>Información General</legend>
             <label for="titulo">Titulo:</label>
@@ -139,8 +195,7 @@ incluirTemplate('header');
                 <?php while ($vendedor = mysqli_fetch_assoc($resultado)):  ?><!--Este while se va ejecutando mientras encuentre resultados en la base de datoa-->
                     <!--Por lo tanto va generando las siguientes opciones-->
                     <!--El value tiene que ser su ID - esto es un operador ternario como un if en una sola linea-->
-                    <option <?php echo $vendedorId === $vendedor['id'] ? 'selected' : ''; ?> value="<?php echo $vendedor['id']; ?>">
-                        <?php echo $vendedor['nombre'] . " " . $vendedor['apellido']; ?>
+                    <option <?php echo $vendedorId === $vendedor['id'] ? 'selected' : ''; ?> value="<?php echo $vendedor['id']; ?>"> <?php echo $vendedor['nombre'] . " " . $vendedor['apellido']; ?>
                     </option><!--Concatenamos  el apellido--->
                 <?php endwhile; ?>
             </select>
