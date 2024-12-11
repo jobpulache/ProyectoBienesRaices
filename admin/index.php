53b05ae1df89c7c3f789543830a2712173a28f0c
Job <pulachecarrenojob837@gmail.com>
2024-12-10 22:25:01 -0500

    creating table for properties list in the dataBase

--- admin/index.php
+++ admin/index.php
@@ -13,6 +13,30 @@ incluirTemplate('header');
         <p class="alerta exito">Anuncio creado correctamente</p>
     <?php endif; ?>
     <a href="/admin/propiedades/crear.php" class="boton boton-verde">Nueva propiedad</a>
+
+    <table class="propiedades">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Título</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>1</td>
+                <td>Home in the Beach</td>
+                <td><img src="/imagenes/2a8783bfefa41e1214ef839586431e09.jpg" alt="Home in the Beach" class="imagen-tabla" height="100"></td>
+                <td>S/.2345678</td>
+                <td>
+                    <a href="#" class="boton-rojo-block">Eliminar</a>
+                    <a href="#" class="boton-amarillo-block">Actualizar</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </main>
 
 
