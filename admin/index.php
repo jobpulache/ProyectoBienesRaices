9eb71357e0283554a70f79e51355ee5c9771beed
Job <pulachecarrenojob837@gmail.com>
2024-12-10 23:33:48 -0500

    add property list

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,22 @@
 <?php
+//Importar la conexión a la base de datos - importing database connection
+require '../includes/config/database.php'; //Eb esta caso solo sale una vez. Pues esta dentro de admin
+$db = conectarBD();
+
+//Escribir el query - write query
+$query = "SELECT * FROM propiedades";
+
+
+//Consultar BD - consult BD
+$resultadoConsulta = mysqli_query($db, $query); //mysqli primero toma la conexión y luego el query
+
+
+
+
+//Sample Message conditional - mostrar mensaje condicional
 $resultado = $_GET['resultado' ?? null]; //Super global get - maneja datoa URL   //Buscca ese valor y si no existe, entonces le asigma null
 
-
+//Unclude template - incluye template
 require '../includes/funciones.php';
 incluirTemplate('header');
 ?>
@@ -24,22 +39,30 @@ incluirTemplate('header');
                 <th>Acciones</th>
             </tr>
         </thead>
-        <tbody>
+
+        <tbody><!--Mostrar los resultados - sample result-->
+            <!--Cod que va a iterar en la base de datos-->
+            <?php  while($propiedad = mysqli_fetch_assoc($resultadoConsulta)): ?>
             <tr>
-                <td>1</td>
-                <td>Home in the Beach</td>
-                <td><img src="/imagenes/2a8783bfefa41e1214ef839586431e09.jpg" alt="Home in the Beach" class="imagen-tabla" height="100"></td>
-                <td>S/.2345678</td>
+                <td><?php echo $propiedad['id'];  ?></td>
+                <td><?php echo $propiedad['titulo']; ?></td>
+                <td><img src="/imagenes/<?php echo $propiedad['imagen']; ?>" alt="Home in the Beach" class="imagen-tabla" height="100"></td>
+                <td><?php echo $propiedad['precio'] ?></td>
                 <td>
                     <a href="#" class="boton-rojo-block">Eliminar</a>
                     <a href="#" class="boton-amarillo-block">Actualizar</a>
                 </td>
             </tr>
+            <?php  endwhile; ?>
         </tbody>
     </table>
 </main>
 
 
+
 <?php
+//Cerrar la conexion - closing connection
+mysqli_close($db);
+
 incluirTemplate('footer');
 ?>
